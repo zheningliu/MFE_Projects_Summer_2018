e792f2bec68f7137e53ce7832faf3fc61873168c
aceninja <lzn_nini@hotmail.com>
2018-06-13 02:22:35 -0700

    ToolBox bug fix + jupyter notebook update

--- 230I/Fixed_Income_Toolbox.py
+++ 230I/Fixed_Income_Toolbox.py
@@ -73,54 +73,67 @@ class ZeroCouponBond:
         return (np.power(ret_T2 / ret_T1, 1 / (k * (T2 - T1))) - 1) * k
 
     
-    def get_par_yield(self, spot, t = 0, k = 1, coupon_t = 0, coupon_k = 2):
+    def get_par_yield(self, spot, t = 0, k = 1, coupon_k = 2):
         """Returns par yield curve of a bond"""
         coupon_freq = 1 / coupon_k
-        first_coupon = coupon_t + coupon_freq
         try:
             T = [item for item in self.maturity]
         except TypeError:
             T = [self.maturity]
             spot = [spot]
-        maturity_dict = dict.fromkeys(T, 0)
-        for m, s in zip(T, spot):
-            n_c = max(math.floor((m - first_coupon) / coupon_freq) + 1, 0)
-            t_c = pd.Series([coupon_t + (i+1) * coupon_freq for i in range(n_c)]) if n_c > 0 else pd.Series([])
-            maturity_dict[m] = self.get_discount_function(s, t_c, t, k)
-            maturity_dict[m] = maturity_dict[m].append(pd.Series(self.get_discount_function(s, m, t, k)))
-        return list(map(lambda x: k * (1 - maturity_dict[x].tolist()[-1]) / sum(maturity_dict[x]), T))
+        maturity_dict = dict(zip(T, spot))
+        z_dict = dict.fromkeys(T, 0)
+        coupon_dict = dict.fromkeys(T, 0)
+        for key in z_dict:
+            n_c = math.floor(key / coupon_freq)
+            t_c = pd.Series([(i+1) * coupon_freq for i in range(n_c)])
+            interp_spot = np.interp(t_c, T, spot)
+            z_dict[key] = self.get_discount_function(interp_spot, t_c, t, k)
+            last_z = pd.Series(self.get_discount_function(maturity_dict[key], key, t, k))
+            z_dict[key] = z_dict[key].append(last_z)
+            accrued = key % coupon_freq / coupon_freq
+            coupon_dict[key] = z_dict[key].append(last_z * accrued)
+        return list(map(lambda x: k * (1 - z_dict[x].tolist()[-1]) / sum(coupon_dict[x]), T))
 
 
     def nelson_siegel_estimation(self, true_price, k = 1, variation = False):
         """Returns nelson siegel term structure estimation"""
-        def NSEerror(params):
+        def function(params):
             exp_t1 = np.exp(- self.maturity / params[3])
-            exp_t2 = np.exp(- self.maturity / params[4])
             common_term = params[0] + params[1] * (1 - exp_t1) / self.maturity * params[3]
             if not variation:
                 diff_term = params[2] * ((1 - exp_t1) / self.maturity * params[3] - exp_t1)
             else:
+                exp_t2 = np.exp(- self.maturity / params[4])
                 diff_term = params[2] * ((1 - exp_t2) / self.maturity * params[4] - exp_t2)
             est_spot = common_term + diff_term
-            est_price= self.get_price(est_spot, k)
-            error = np.dot(np.power(est_price - true_price, 2),1 / self.maturity)
+            return self.get_price(est_spot, k)
+
+        def NSEerror(params):
+            est_price= function(params)
+            error = np.dot(np.power(est_price - true_price, 2), 1 / self.maturity)
             return error 
-        params = np.array([0.5, 0.1, 0.3, 1.0, 0.2]) if variation else np.array([0, 0, 0, 1])
-        res = minimize(NSEerror, params, method = 'nelder-mead', options = {'xtol': 0.00001, 'disp': True})
-        return res.x
+
+        params = np.array([0.01, -0.01, 0.01, 5.0, 0.5]) if variation else np.array([0, 0, 0, 1])
+        res = minimize(NSEerror, params, method = 'nelder-mead', options = {'maxiter': 10000, 'xtol': 0.00001, 'disp': True})
+        return function(res.x)
     
 
     def svensson_estimation(self, true_price, k = 1):
         """Returns svensson term structure estimation"""
-        def SSEerror(params):
+        def function(params):
             exp_t1 = np.exp(- self.maturity / params[4])
             exp_t2 = np.exp(- self.maturity / params[5])
             est_spot = params[0] + params[1] * (1 - exp_t1) / self.maturity * params[4] + \
                         params[2] * ((1 - exp_t1) / self.maturity * params[4] - exp_t1) + \
                         params[3] * ((1 - exp_t2) / self.maturity * params[5] - exp_t2)
-            est_price= self.get_price(est_spot, k)
+            return self.get_price(est_spot, k)
+
+        def SSEerror(params):
+            est_price = function(params)
             error = np.dot(np.power(est_price - true_price, 2),1 / self.maturity)
-            return error 
+            return error
+
         params = np.array([0, 0, 0, 1, 0, 2])
-        res = minimize(SSEerror, params, method = 'nelder-mead', options = {'xtol': 0.0001, 'disp': True})
-        return res.x
+        res = minimize(SSEerror, params, method = 'nelder-mead', options = {'maxiter': 10000, 'xtol': 0.0001, 'disp': True})
+        return function(res.x)
